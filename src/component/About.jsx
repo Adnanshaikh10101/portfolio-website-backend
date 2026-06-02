0320cb0635326df524bbbded495b54b61e5ffef6
Adnan Shaikh <shaikhaadnan02@gmail.com>
2026-06-02 19:15:04 +0530

    About Changes

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -3,7 +3,20 @@ function About(){
     return(
         <div className="about-section">
             <div className="About">
-
+                <h1>About Me</h1>
+                <p>Hello Everyone I Am Adnan Shaikh from Mumbai.
+                    I am looking for Freelancing Web Tasks and Web Developer 
+                    Internship either Job.
+                </p>
+                <h4>
+                    I currently completed my Second Year In bsc(Computer Science)
+                    Through The ICLES Motilal Jhunjhunwala college Vashi Navi-Mumbai.
+                    also I have a Great Knowledge About Web Development 
+                </h4>
+                <h3>
+                    I have knowledge About HTML,CSS,JAVASCRIPT,EXPRESSJS,NODEJS,
+                    ANGULARJS,REACTJS,MONGODB,RESTAPI,RESENDAPI ETC and also 
+                </h3>
             </div>
         </div>
     );
